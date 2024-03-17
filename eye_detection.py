cc6469d2e9075cf18193f7728756624f27c2fd88
NAVEEN PRAKASH <naveen.prakash.kv@gmail.com>
2024-03-17 14:59:18 +0530

    eye

--- eye_detection.py
+++ eye_detection.py
@@ -13,18 +13,6 @@ for (x,y,w,h) in face:
     cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),5)
     
 
-# Load the pre-trained face detection cascade
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-# Detect faces in the grayscale image
-faces = face_cascade.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
-
-
-# Draw rectangles around the detected faces
-for (x, y, w, h) in faces:
-    cv.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-
-# Display the image with detected faces
-cv.imshow('Detected Faces', img)
+cv.imshow("eye",img)
 cv.waitKey(0)
 cv.destroyAllWindows()
